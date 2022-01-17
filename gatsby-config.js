a3992ac5032b89fef9d6a2eed8cf11266b72e429
rushfe <2957720468@qq.com>
2022-01-17 20:49:01 +0800

    Img and Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    siteMetadata: {
-      title: `My Gatsby Site`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
